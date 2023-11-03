58a0370dcfdd1820e8a1696ea9f934259c5ee410
Mateuzz <vactusorigin@gmail.com>
2023-11-03 00:51:27 -0300

    Overhaul

--- main.php
+++ main.php
@@ -6,19 +6,18 @@ require "Mysql.php";
 use CpChart\Data;
 use CpChart\Image;
 
-const IMAGE_WIDTH = 1600;
+const IMAGE_WIDTH = 1700;
 const IMAGE_HEIGHT = 600;
 const OUTER_PADDING = 100;
 
-$legendConfig = [ 'Mode' => LEGEND_HORIZONTAL, "R" => 120, "G" => 120, "B" => 180, ];
-$barConfig = [ "Rounded" => true, "Surrounding" => 30, "DisplayValues" => true, "DisplayPos" => LABEL_POS_OUTSIDE, "Gradient" => true ];
+$legendConfig = ['Mode' => LEGEND_HORIZONTAL, "R" => 100, "G" => 100, "B" => 100, 'BoxWidth' => 16, 'BoxHeight' => 16, "Alpha" => 20, "FontSize" => 18, "Family" => LEGEND_FAMILY_CIRCLE];
+$barConfig = ["Rounded" => true, "Surrounding" => 30, "DisplayValues" => false, "DisplayPos" => LABEL_POS_OUTSIDE, "Gradient" => true];
 
 $db = getDatabase();
 
 $data = [];
 $data2 = [];
 $dataModelLoading = [];
-$dataStartup = [];
 $dataCPU = [];
 $dataRAM = [];
 
@@ -41,14 +40,6 @@ $camps = [
     ["fps_max_avg", "max"],
     ["fps_min_avg", "min"],
     ["fps_low", "low"],
-];
-
-$modelTestsScenes = [
-    'Forte',
-    'Floresta',
-    'Floresta (Combinado)',
-    'Crânio',
-    'Deserto'
 ];
 
 $query = "select ";
@@ -76,19 +67,13 @@ while ($row = $result->fetch_assoc()) {
     }
 }
 
-/* makeBarCharts($data, $data2, $libraries); */
-
-$cpuTestLabels = [
-    'linux' => ['FP', 'FP-C', 'FL', 'FL-COM', 'FL-SO', 'FL-C-FX-S', 'C'],
-    'windows' => ['FP', 'FP-C', 'FL', 'FL-28', 'FL-SO', 'FL-C-FX-S', 'C', 'DE'],
-];
 
 $dataNPM = [
     'Código' => [
-        'Three' => [1165,670,184],
-        'Babylon' => [6800,4800,1056],
-        'claygl' => [870,440,124],
-        'pc' => [2500,1400,372],
+        'Three' => [1165, 670, 184],
+        'Babylon' => [6800, 4800, 1056],
+        'claygl' => [870, 440, 124],
+        'pc' => [2500, 1400, 372],
     ],
     'NPM' => [
         'Three' => [32, 32],
@@ -99,9 +84,8 @@ $dataNPM = [
 ];
 
 
-/* makeNPMCharts($dataNPM, $libraries); */
-
-function makeNPMCharts($data, $libraries) {
+function makeNPMCharts($data, $libraries)
+{
     $npmTestLabels = [
         'NPM' => ['Principal', 'Total'],
         'Código' => ['Bundled', 'Minificado', 'Minificado + GZIP'],
@@ -113,6 +97,10 @@ function makeNPMCharts($data, $libraries) {
     ];
 
     global $barConfig, $legendConfig;
+
+    $npmBarConfig = array_merge($barConfig, [
+        "DisplayValues" => true
+    ]);
 
     $image = getImage(IMAGE_WIDTH, IMAGE_HEIGHT);
     $totalWidth = (IMAGE_WIDTH - (OUTER_PADDING * 3));
@@ -140,7 +128,7 @@ function makeNPMCharts($data, $libraries) {
             'Pos' => SCALE_POS_TOPBOTTOM,
             'Factors' => [40]
         ];
-        $image->drawBarChart($barConfig);
+        $image->drawBarChart($npmBarConfig);
         cursorMove($cursor, $width + OUTER_PADDING);
         $width = 0.4 * $totalWidth;
     }
@@ -152,41 +140,54 @@ function makeNPMCharts($data, $libraries) {
 $dataCPU = [
     'linux' => [
         'CPU' => [
-            'pc' => [15,VOID,45,20,30,VOID,11],
-            'Three' => [15,15,50,15,50,50,15],
-            'Babylon' => [15,15,50,15,55,50,20],
-            'claygl' => [20,30,50,15,100,100,20],
+            'pc' => [15, VOID, 45, 15, 50, VOID, 20],
+            'Three' => [15, 15, 50, 20, 25, 50, 15],
+            'Babylon' => [15, 15, 50, 55, VOID, 50, 15],
+            'claygl' => [20, 30, 50, 100, 40,100, 15],
         ],
         'RAM' => [
-            'pc' => [12,VOID,41,11,38,VOID,11],
-            'Three' => [6,10,20,6,8,25,8],
-            'Babylon' => [15,25,45,16,30,48,26],
-            'claygl' => [6,13,17,8,20,29,24],
+            'pc' => [12, VOID, 41, 32, 38, VOID, 11],
+            'Three' => [6,10,20,8,26,25,6],
+            'Babylon' => [15,25,45,30,VOID,48,16],
+            'claygl' => [6,13,17,20,20,29,8],
         ]
     ],
     'windows' => [
         'CPU' => [
-            'pc' => [5,VOID,20,30,15,VOID,5,100],
-            'Three' => [5,5,20,30,20,45,5,100],
-            'Babylon' => [5,5,35,20,45,20,5,100],
-            'claygl' => [5,5,30,45,30,90,5,100],
+            'pc' => [5,VOID,20,30,15,VOID,3,100],
+            'Three' => [5,5,20,30,20,45,3,100],
+            'Babylon' => [5,5,20,45,VOID,35,3,100],
+            'claygl' => [5,5,30,45,30,90,3,100],
         ],
         'RAM' => [
-            'pc' => [20,VOID,37,52,36,VOID,40,100],
-            'Three' => [6.6,8,20,33,30,36,24,42],
-            'Babylon' => [16,18,48,45,50,67,27,126],
-            'claygl' => [15,28,28,30,33,37,22,35],
+            'pc' => [20,VOID,37,52,36,VOID,11,100],
+            'Three' => [6.6,8,20,33,30,36,5,42],
+            'Babylon' => [16,18,45,50,VOID,48,14,126],
+            'claygl' => [15,28,28,30,33,37,6,35],
         ]
     ]
-    ];
+];
+
+function makeCpuCharts($dataCPU, $libraries)
+{
+    global $barConfig, $legendConfig;
 
     $cpuAxisNames = [
         'CPU' => 'CPU (%)',
         'RAM' => 'RAM (MB)'
     ];
 
-    $cpuBarConfig = array_merge($barConfig, ["DisplayValues" => false]);
+    $factors = [
+        'windows' => 15,
+        'linux' => 5,
+    ];
 
+    $cpuTestLabels = [
+        'linux' => ['FP', 'FP-C', 'FL', 'FL-SO', 'FL-28', 'FL-C-FX-S', 'FL-CO'],
+        'windows' => ['FP', 'FP-C', 'FL', 'FL-SO', 'FL-28', 'FL-C-FX-S', 'FL-CO', 'DE'],
+    ];
+
+    $cpuBarConfig = array_merge($barConfig, ["DisplayValues" => false]);
 
     foreach ($dataCPU as $pcName => $pc) {
         $image = getImage(IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -194,9 +195,10 @@ $dataCPU = [
         $width = 0.6 * $totalWidth;
         $height = IMAGE_HEIGHT - 60 - 80;
         $cursor = getCursor();
-        $settings = [];
 
         cursorMove($cursor, OUTER_PADDING, 60);
+
+        $settings = [];
 
         foreach ($pc as $infoName => $infoData) {
             $data = new Data();
@@ -214,7 +216,7 @@ $dataCPU = [
             imageStartDraw($image, $cursor, $data, $settings);
             $settings = [
                 'Pos' => SCALE_POS_TOPBOTTOM,
-                'Factors' => [10]
+                'Factors' => [$factors[$pcName]]
             ];
             $image->drawBarChart($cpuBarConfig);
             cursorMove($cursor, $width + OUTER_PADDING);
@@ -224,8 +226,9 @@ $dataCPU = [
         $image->drawLegend(OUTER_PADDING, IMAGE_HEIGHT - 30, $legendConfig);
         $image->autoOutput("./cpu/$pcName.png");
     }
+}
 
-$query = "select avg(ms) ms, library, scene, pc from model_loading group by pc, scene, library order by scene";
+$query = "select avg(ms) ms, library, scene, pc from model_loading where scene <> 'skull' group by pc, scene, library order by scene";
 $result = $db->query($query);
 
 while ($row = $result->fetch_assoc()) {
@@ -249,85 +252,66 @@ while ($row = $result->fetch_assoc()) {
     $dataModelLoading[$pc]['startup'][$library][1] = (int)$ms;
 }
 
-$modelLoadingTestLabels = [
-    'model_loading' => ['', 'Floresta', 'Deserto', 'Crânio'],
-    'startup' => ['Inicialização'],
-];
 
-$modelLoadingTestLabels = [
-    'linux' => [
-        'startup' => ['Média', 'Mínimo'],
-        'model_loading' => ['Floresta', 'Floresta (Combinado)', 'Forte Pirata', 'Crãnio'],
-    ],
-    'windows' => [
-        'startup' => ['Média', 'Mínimo'],
-        'model_loading' => ['Deserto', 'Floresta', 'Floresta (Combinado)', 'Forte Pirata', 'Crãnio'],
-    ],
-];
-
-foreach ($dataModelLoading as $pcName => $pc) {
-    $image = getImage(IMAGE_WIDTH, IMAGE_HEIGHT);
-    $totalWidth = (IMAGE_WIDTH - (OUTER_PADDING * 3));
-    $width = 0.6 * $totalWidth;
-    $height = IMAGE_HEIGHT - 50 - 80;
-    $cursor = getCursor();
-    $settings = [
-        'DisplayValues' => true,
-        'Factors' => [1000, 500]
+function makeLoadingChart($dataModelLoading, $libraries)
+{
+    $modelLoadingTestLabels = [
+        'linux' => [
+            'startup' => ['Média', 'Mínimo'],
+            'model_loading' => ['Floresta', 'Floresta (Combinado)', 'Forte Pirata'],
+        ],
+        'windows' => [
+            'startup' => ['Média', 'Mínimo'],
+            'model_loading' => ['Deserto', 'Floresta', 'Floresta (Combinado)', 'Forte Pirata'],
+        ],
     ];
-
-    $barConfig = array_merge($barConfig, [
+    global $barConfig, $legendConfig;
+    $LoadingbarConfig = array_merge($barConfig, [
         "DisplayValues" => true
     ]);
 
-    cursorMove($cursor, OUTER_PADDING, 50);
+    foreach ($dataModelLoading as $pcName => $pc) {
+        $image = getImage(IMAGE_WIDTH, IMAGE_HEIGHT);
+        $totalWidth = (IMAGE_WIDTH - (OUTER_PADDING * 3));
+        $width = 0.6 * $totalWidth;
+        $height = IMAGE_HEIGHT - 50 - 80;
+        $cursor = getCursor();
+        $settings = [];
 
-    foreach ($pc as $infoName => $infoData) {
-        $data = new Data();
+        cursorMove($cursor, OUTER_PADDING, 50);
 
-        foreach ($libraries as $lib => $libName) {
-            $data->addPoints($infoData[$lib], $libName);
+        foreach ($pc as $infoName => $infoData) {
+            $data = new Data();
+
+            foreach ($libraries as $lib => $libName) {
+                $data->addPoints($infoData[$lib], $libName);
+            }
+
+            $data->addPoints($modelLoadingTestLabels[$pcName][$infoName], 'labels');
+            $data->setAbscissa('labels');
+            $data->setAxisName(0, [
+                'startup' => 'Inicialização (ms)',
+                'model_loading' => 'Carregamento de Modelos (ms)'
+            ][$infoName]);
+
+            cursorSetSize($cursor, $width, $height);
+            imageStartDraw($image, $cursor, $data, $settings);
+            $settings = [
+                'Pos' => SCALE_POS_TOPBOTTOM,
+                'Factors' => [60]
+            ];
+            $image->drawBarChart($LoadingbarConfig);
+            cursorMove($cursor, $width + OUTER_PADDING);
+            $width = 0.4 * $totalWidth;
         }
 
-        $data->addPoints($modelLoadingTestLabels[$pcName][$infoName], 'labels');
-        $data->setAbscissa('labels');
-        $data->setAxisName(0, [
-            'startup' => 'Inicialização (ms)',
-            'model_loading' => 'Carregamento de Modelos (ms)'
-        ][$infoName]);
-
-        cursorSetSize($cursor, $width, $height);
-        imageStartDraw($image, $cursor, $data, $settings);
-        $settings = [
-            'Pos' => SCALE_POS_TOPBOTTOM,
-        ];
-        $image->drawBarChart($barConfig);
-        cursorMove($cursor, $width + OUTER_PADDING);
-        $width = 0.4 * $totalWidth;
+        $image->drawLegend(OUTER_PADDING, IMAGE_HEIGHT - 30, $legendConfig);
+        $image->autoOutput("./loading/$pcName.png");
     }
-
-    $image->drawLegend(OUTER_PADDING, IMAGE_HEIGHT - 30, $legendConfig);
-    $image->autoOutput("./loading/$pcName.png");
 }
 
-$query = "select avg(ms) ms, scene, pc, library from model_loading group by scene, pc, library";
-$result = $db->query($query);
-
-while ($row = $result->fetch_assoc()) {
-    extract($row);
-    $dataModelLoading[$scene][$pc][$library] = $ms;
-}
-
-$query = "select avg(ms) ms, scene, pc, library from startup group by scene, pc, library";
-$result = $db->query($query);
-
-while ($row = $result->fetch_assoc()) {
-    extract($row);
-    $dataStartup[$pc][$library] = $ms;
-}
-
-
-function makeBarCharts($data, $data2, $libraries) {
+function makeBarCharts($data, $data2, $libraries)
+{
     foreach ($data2 as $browserName => $browser) {
         foreach ($browser as $extraGroupName => $extraGroup) {
             makeRenderingChart("./images/$browserName-$extraGroupName.png", $extraGroup, $libraries);
@@ -341,20 +325,22 @@ function makeBarCharts($data, $data2, $libraries) {
     }
 }
 
-function getImage($width, $height) {
-    $image = new Image(IMAGE_WIDTH, IMAGE_HEIGHT);
-    $image->drawRectangle(1, 1, IMAGE_WIDTH - 1, IMAGE_HEIGHT -1, makeArrayColor(0, 0, 0));
+function getImage($width, $height)
+{
+    $image = new Image($width, $height);
+    $image->drawRectangle(1, 1, $width - 1, $height - 1, makeArrayColor(0, 0, 0));
 
     $Settings = array("R" => 170, "G" => 183, "B" => 87, "Dash" => 1, "DashR" => 190, "DashG" => 203, "DashB" => 107);
-    $image->drawFilledRectangle(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, $Settings);
+    $image->drawFilledRectangle(0, 0, $width, $height, $Settings);
 
     $Settings = array("StartR" => 219, "StartG" => 231, "StartB" => 139, "EndR" => 1, "EndG" => 138, "EndB" => 68, "Alpha" => 50);
-    $image->drawGradientArea(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, DIRECTION_VERTICAL, $Settings);
+    $image->drawGradientArea(0, 0, $width, $height, DIRECTION_VERTICAL, $Settings);
 
     return $image;
 }
 
-function getData(array $pointData, ?string $abcissaName) {
+function getData(array $pointData, ?string $abcissaName)
+{
     $data = new Data();
     foreach ($pointData as $points) {
         $data->addPoints($points['values'], $points['name']);
@@ -364,24 +350,28 @@ function getData(array $pointData, ?string $abcissaName) {
     return $data;
 }
 
-function getCursor() {
+function getCursor()
+{
     return [
         'xStart' => 0,
         'yStart' => 0,
     ];
 }
 
-function cursorSetSize(&$layout, $width, $height) {
+function cursorSetSize(&$layout, $width, $height)
+{
     $layout['xEnd'] = $layout['xStart'] + $width;
     $layout['yEnd'] = $layout['yStart'] + $height;
 }
 
-function cursorMove(&$layout, $x = 0, $y = 0) {
+function cursorMove(&$layout, $x = 0, $y = 0)
+{
     $layout['xStart'] += $x;
     $layout['yStart'] += $y;
 }
 
-function imageStartDraw(Image $image, &$cursor, $data, $settings =[]) {
+function imageStartDraw(Image $image, &$cursor, $data, $settings = [])
+{
     $image->setDataSet($data);
     $drawSettings = [
         "CycleBackground" => true,
@@ -397,19 +387,21 @@ function imageStartDraw(Image $image, &$cursor, $data, $settings =[]) {
 
     extract($cursor);
 
-    $image->setFontProperties(["FontName" => "verdana.ttf", "FontSize" => 12]);
+    $image->setFontProperties(["FontName" => "verdana.ttf", "FontSize" => 16]);
     $image->setGraphArea($xStart, $yStart, $xEnd, $yEnd);
     $image->drawScale($drawSettings);
     $image->setShadow(true, ["X" => 1, "Y" => 1, "R" => 0, "G" => 0, "B" => 0, "Alpha" => 10]);
 }
 
 
-function makeArrayColor($R, $G, $B) {
+function makeArrayColor($R, $G, $B)
+{
     return compact(["R", "G", "B"]);
 }
 
 
-function makeRenderingChart($filename, $group, $libraries) {
+function makeRenderingChart($filename, $group, $libraries)
+{
     $count = count($group);
     $image = getImage(IMAGE_WIDTH, IMAGE_HEIGHT);
     $width = (IMAGE_WIDTH - OUTER_PADDING * ($count + 1)) / $count;
@@ -430,7 +422,6 @@ function makeRenderingChart($filename, $group, $libraries) {
 
         $data->addPoints($renderingTestLabels, 'fps');
         $data->setAbscissa('fps');
-        /* $data->setAxisName(0, "FPS"); */
 
         cursorSetSize($cursor, $width, $height);
         imageStartDraw($image, $cursor, $data);
@@ -441,3 +432,8 @@ function makeRenderingChart($filename, $group, $libraries) {
     $image->drawLegend(OUTER_PADDING, IMAGE_HEIGHT - 30, $legendConfig);
     $image->autoOutput($filename);
 }
+
+/* makeNPMCharts($dataNPM, $libraries); */
+/* makeBarCharts($data, $data2, $libraries); */
+makeLoadingChart($dataModelLoading, $libraries);
+/* makeCpuCharts($dataCPU, $libraries); */
